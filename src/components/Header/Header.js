ca158cee51f02ce488e885b18130e36645f54af3
Talha <noname_2338@hotmail.com>
2021-03-11 21:18:29 +0100

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './images/img.png';
+import search from './images/search.png';
 
 const Header = () => {
 	return (
 		<div
 			style={{
-				width: '100%',
+				width: '1152px',
 				height: '104px',
+				left: "0px",
+				top: "1px"
 			}}
 		>
 			<img
@@ -18,6 +21,21 @@ const Header = () => {
 					top: '34px',
 				}}
 				src={logo}
+				alt="logo"
+			/>
+			<img
+				style={{
+					position: "absolute",
+					left: "95.57%",
+					right: "2.87%",
+					top: "1.96%",
+					bottom: "97.32%",
+					background: "#FFFFFF",
+					border: "1px solid #347CA5",
+					boxSizing: "border-box",
+
+				}}
+				src={search}
 				alt="logo"
 			/>
 			<button
@@ -45,9 +63,88 @@ const Header = () => {
 						color: 'white',
 					}}
 				>
+					
 					DONATE
 				</span>
+				
 			</button>
+			<span
+				style={{
+					position: "absolute",
+					width: "134px",
+					height: "20px",
+					left: "256px",
+					top: "49px",
+					fontFamily: "Roboto",
+					fontStyle: "normal",
+					fontWeight: "normal",
+					fontSize: "24px",
+					lineHeight: "28px",
+
+					color: "#347CA5"
+
+				}}>Get involved</span>
+				<span
+				style={{
+					position: "absolute",
+					width: "217px",
+					height: "20px",
+					left: "418px",
+					top: "48px",
+					fontFamily: "Roboto",
+					fontStyle: "normal",
+					fontWeight: "normal",
+					fontSize: "24px",
+					lineHeight: "28px",
+
+					color: "#347CA5"
+
+				}}>Campaigns & News</span>
+				<span
+				style={{
+					position: "absolute",
+					width: "103px",
+					height: "20px",
+					left: "663px",
+					top: "46px",
+					fontFamily: "Roboto",
+					fontStyle: "normal",
+					fontWeight: "normal",
+					fontSize: "24px",
+					lineHeight: "28px",
+
+					color: "#347CA5"
+
+				}}>About us</span>
+				<span
+				style={{
+					position: "absolute",
+					width: "84px",
+					height: "27px",
+					left: "794px",
+					top: "47px",
+					fontFamily: "Roboto",
+					fontStyle: "normal",
+					fontWeight: "normal",
+					fontSize: "24px",
+					lineHeight: "28px",
+
+					color: "#347CA5"
+
+				}}>Log in</span>
+				<input type="text" placeholder="Search" style={{
+					position: "absolute",
+					width: "209px",
+					height: "27px",
+					left: "917px",
+					top: "43px",
+					background: "#FFFFFF",
+					border: "1px solid #347CA5",
+					boxSizing: "border-box",
+					borderRadius: "10px",
+					color: "#347CA5"
+
+				}}/>
 		</div>
 	);
 };
