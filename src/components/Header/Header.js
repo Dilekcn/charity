b8c407ea136d460cc6adfe9600431bd9f9676e6e
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-11 21:47:43 +0000

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './images/img.png';
 import search from './images/search.png';
+import './header.css';
 
 const Header = () => {
 	return (
@@ -8,8 +9,8 @@ const Header = () => {
 			style={{
 				width: '1152px',
 				height: '104px',
-				left: "0px",
-				top: "1px"
+				left: '0px',
+				top: '1px',
 			}}
 		>
 			<img
@@ -17,33 +18,19 @@ const Header = () => {
 					position: 'absolute',
 					width: '48px',
 					height: '44px',
-					left: '21px',
+					left: '41px',
 					top: '34px',
 				}}
 				src={logo}
 				alt="logo"
 			/>
-			<img
-				style={{
-					position: "absolute",
-					left: "95.57%",
-					right: "2.87%",
-					top: "1.96%",
-					bottom: "97.32%",
-					background: "#FFFFFF",
-					border: "1px solid #347CA5",
-					boxSizing: "border-box",
 
-				}}
-				src={search}
-				alt="logo"
-			/>
 			<button
 				style={{
 					position: 'absolute',
 					width: '153px',
 					height: '63px',
-					left: '88px',
+					left: '118px',
 					top: '22px',
 					background: '#347CA5',
 					borderRadius: '10px',
@@ -63,88 +50,60 @@ const Header = () => {
 						color: 'white',
 					}}
 				>
-					
 					DONATE
 				</span>
-				
 			</button>
-			<span
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<a href="#getInvolved">Get Involved</a>
+						</li>
+						<li>
+							<a href="#campaings&news">Campaigns & News</a>
+						</li>
+						<li>
+							<a href="#aboutus">About Us</a>
+						</li>
+						<li>
+							<a href="#login">Log In</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<input
+				type="text"
+				placeholder="Search"
 				style={{
-					position: "absolute",
-					width: "134px",
-					height: "20px",
-					left: "256px",
-					top: "49px",
-					fontFamily: "Roboto",
-					fontStyle: "normal",
-					fontWeight: "normal",
-					fontSize: "24px",
-					lineHeight: "28px",
-
-					color: "#347CA5"
-
-				}}>Get involved</span>
-				<span
+					position: 'absolute',
+					width: '279px',
+					height: '27px',
+					left: '1057px',
+					top: '40px',
+					background: '#FFFFFF',
+					border: '1px solid #347CA5',
+					boxSizing: 'border-box',
+					borderRadius: '10px',
+					color: '#347CA5',
+				}}
+			/>
+			<img
+				id="img"
 				style={{
-					position: "absolute",
-					width: "217px",
-					height: "20px",
-					left: "418px",
-					top: "48px",
-					fontFamily: "Roboto",
-					fontStyle: "normal",
-					fontWeight: "normal",
-					fontSize: "24px",
-					lineHeight: "28px",
-
-					color: "#347CA5"
-
-				}}>Campaigns & News</span>
-				<span
-				style={{
-					position: "absolute",
-					width: "103px",
-					height: "20px",
-					left: "663px",
-					top: "46px",
-					fontFamily: "Roboto",
-					fontStyle: "normal",
-					fontWeight: "normal",
-					fontSize: "24px",
-					lineHeight: "28px",
-
-					color: "#347CA5"
-
-				}}>About us</span>
-				<span
-				style={{
-					position: "absolute",
-					width: "84px",
-					height: "27px",
-					left: "794px",
-					top: "47px",
-					fontFamily: "Roboto",
-					fontStyle: "normal",
-					fontWeight: "normal",
-					fontSize: "24px",
-					lineHeight: "28px",
-
-					color: "#347CA5"
-
-				}}>Log in</span>
-				<input type="text" placeholder="Search" style={{
-					position: "absolute",
-					width: "209px",
-					height: "27px",
-					left: "917px",
-					top: "43px",
-					background: "#FFFFFF",
-					border: "1px solid #347CA5",
-					boxSizing: "border-box",
-					borderRadius: "10px",
-					color: "#347CA5"
-
-				}}/>
+					position: 'absolute',
+					left: '93.97%',
+					right: '2.87%',
+					top: '6.36%',
+					bottom: '97.32%',
+					background: '#FFFFFF',
+					border: '0px #347CA5',
+					boxSizing: 'border-box',
+					width: '17.91px',
+					height: '17.32px',
+				}}
+				src={search}
+				alt="logo"
+			/>
 		</div>
 	);
 };
