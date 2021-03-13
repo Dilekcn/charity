6a78c5ffd5b42c03b1d9cea4827256cb62dbb76c
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-13 17:38:51 +0000

    updated donate and header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,29 @@
 import React from 'react';
-<<<<<<< HEAD
 import Donate from '../Donate/Donate';
-=======
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from '../Auth/Login';
 import Footer from '../Footer/Footer';
->>>>>>> e98fa1e4a4679608a13a6f73f661342176d59c73
+
 import Header from '../Header/Header';
 import NewsHomePage from '../News-HomePage/NewsHomePage';
 import SignUp from '../Auth/SignUp';
 
 export default function App() {
 	return (
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 		<div>
 			<Header />
-			{/* <Donate/> */}
-		</div>
-=======
-		<Router>
-			<div className="app">
-				<Header />
-				<NewsHomePage />
-=======
-		<div>
-			<Header />
-			<NewsHomePage />
+			{/* <NewsHomePage /> */}
 			<Router>
->>>>>>> Stashed changes
 				<Switch>
 					<Route exact path="/login" component={Login} />
 					<Route exact path="/signup" component={SignUp} />
+					<Route exact path="/donate" component={Donate} />
 				</Switch>
-<<<<<<< Updated upstream
+
 				<Footer />
-			</div>
-		</Router>
->>>>>>> e98fa1e4a4679608a13a6f73f661342176d59c73
-=======
 			</Router>
-			<Footer />
 		</div>
->>>>>>> Stashed changes
 	);
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,18 @@
 import React from 'react';
 import logo from './images/img.png';
-<<<<<<< HEAD
-import search from './images/search.png';
-import './Header.css';
-
-const Header = () => {
-	return (
-		<div className="div-header">
-			<img id="img-logo" src={logo} alt="logo" />
-			<button id="btn-donate">
-				<span id="text-btn-donate">DONATE</span>
-			</button>
-=======
 import './Header.css';
 
 const Header = () => {
 	return (
 		<div id="div-header">
-<<<<<<< Updated upstream
->>>>>>> e98fa1e4a4679608a13a6f73f661342176d59c73
-			<nav>
-				<a href="#home">
-					<img id="img-logo" src={logo} alt="logo" />
-				</a>
-				<a href="#donate">
-					<button id="btn-donate">
-						<span id="text-btn-donate">DONATE</span>
-					</button>
-=======
 			<div>
-				<img src={logo} alt="logo" />
+				<a href="http://localhost:3000/">
+					<img src={logo} alt="logo" />
+				</a>
 			</div>
 			<div>
-				<a href="/#">
+				<a href="http://localhost:3000/donate">
 					<button id="btn-donate">DONATE</button>
->>>>>>> Stashed changes
 				</a>
 			</div>
 			<div>
