3b03d0b180f20616264b54c096e278d475a1c042
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-13 15:57:15 +0000

    updated footer

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Login.css';
-import logo from './sign-in-logo.jpg'
+import logo from './sign-in-logo.jpg';
 
 const Login = () => {
 	return (
@@ -14,12 +14,28 @@ const Login = () => {
 				</div>
 			</div>
 			<div className="sign-in-body">
-                 <h1 className="sign-in-SignIn">Sign in</h1>
+				<h1 className="sign-in-SignIn">Sign in</h1>
 				<form className="sign-in-form">
-					<input className="sign-in-email" type="text" name="email" placeholder="Enter your email" />
-					<input className="sign-in-password" type="password" name="password" placeholder="Enter your password" />
-                    <p className="sig-in-forgot-password"><a href="#">Forgot your password?</a></p>
-					<input className="sign-in-submit" type="submit" value="Sign in" />
+					<input
+						className="sign-in-email"
+						type="text"
+						name="email"
+						placeholder="Enter your email"
+					/>
+					<input
+						className="sign-in-password"
+						type="password"
+						name="password"
+						placeholder="Enter your password"
+					/>
+					<p className="sig-in-forgot-password">
+						<a href="#pass">Forgot your password?</a>
+					</p>
+					<input
+						className="sign-in-submit"
+						type="submit"
+						value="Sign in"
+					/>
 				</form>
 				<div className="sign-in-signUp">
 					<p>
