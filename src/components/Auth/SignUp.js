db858a55440c5089e8cb387e6b79c83c34f2ded1
blackeagle94 <blackeagle4894@gmail.com>
2021-03-14 23:36:44 +0200

    ismail

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -1,36 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SignUp.css';
 import googleLogo from './grommet-icons_google.jpg';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const SignUp = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirm, setConfirm] = useState('')
+	const [existEmail, setExistEmail] = useState(true)
+	const [users, setUsers] = useState([])
+
+	const signup = () => {
+		if(email.trim() == '' || password.trim() == '' || confirm.trim() == '') {
+			document.querySelector('.valid').textContent = 'Please fill the blanks!'
+			return
+		}
+
+		if(users.length > 0) {
+			for(let i =0; i < users.length; i++) {
+				if(users.data[i].email.toLowerCase() == email.toLowerCase()) {
+					document.querySelector('.valid').textContent = 'Email is already taken'
+					setExistEmail(false)
+					return
+				} else if (password !== confirm){
+					document.querySelector('.valid').textContent = 'Password did not match'
+					setExistEmail(false)
+					return 
+				}
+			}
+		}
+
+		if(!existEmail) {
+			axios.post('http://localhost:4000/users', {
+						email:email,
+						password:password
+					})
+					.then(res => console.log(res.data)).catch(err => console.log(err))
+		}
+		setExistEmail(true)
+	}
+
+	useEffect(() => {
+		axios.get('http://localhost:4000/users')
+		.then(res => {
+			setUsers(res.data)
+		})
+		.catch(err => console.log(err))
+	},[])
+
 	return (
 		<div className="sign-up">
 			<div className="sign-up-body">
 				<h1 className="sign-up-SignIn">Sign up</h1>
+				<h3 className="valid" style={{color:'red'}}></h3>
 				<form className="sign-up-form">
 					<input
 						className="sign-up-email"
-						type="text"
+						type="email"
 						name="email"
+						value={email}
 						placeholder="Enter your email"
+						onChange={(e) => setEmail(e.target.value)}
 					/>
 					<input
 						className="sign-up-password"
 						type="password"
 						name="password"
+						value={password}
 						placeholder="Enter your password"
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 					<input
 						className="sign-up-password"
 						type="password"
 						name="password"
+						value={confirm}
 						placeholder="Confirm your password"
+						onChange={(e) => setConfirm(e.target.value)}
 					/>
-					<input
-						className="sign-up-submit"
-						type="submit"
-						value="Sign up"
-					/>
+					<Link className="link" onClick={signup}>Sign up</Link>
 					<button className="sign-up-google" type="button">
 						<img src={googleLogo} alt="google logo" /> Join with Google
 					</button>
