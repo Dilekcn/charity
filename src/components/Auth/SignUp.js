6720f4e438d503c49ec28e1a33b864e9d4058780
blackeagle94 <blackeagle4894@gmail.com>
2021-03-17 13:28:20 +0200

    added google auth

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -3,66 +3,77 @@ import './SignUp.css';
 import googleLogo from './grommet-icons_google.jpg';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import GoogleLogin from 'react-google-login';
 
 const SignUp = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [confirm, setConfirm] = useState('')
-	const [existEmail, setExistEmail] = useState(true)
-	const [users, setUsers] = useState(null)
-	const [refresh, setRefresh] = useState(false)
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ confirm, setConfirm ] = useState('');
+	const [ existEmail, setExistEmail ] = useState(true);
+	const [ users, setUsers ] = useState(null);
+	const [ refresh, setRefresh ] = useState(false);
 
-	useEffect(() => {
-		axios.get('http://localhost:4000/users')
-		.then(res => {
-			setUsers(res.data)
-		})
-		.catch(err => console.log(err))
-	},[refresh])
+	const responseGoogle = (response) => {
+		console.log(response);
+		console.log(response.profileObj);
+	};
+
+	useEffect(
+		() => {
+			axios
+				.get('http://localhost:4000/users')
+				.then((res) => {
+					setUsers(res.data);
+				})
+				.catch((err) => console.log(err));
+		},
+		[ refresh ]
+	);
 
 	const signup = (e) => {
-		e.preventDefault()
-		document.querySelector('.valid').textContent = ''
-		if(email.trim() == '' || password.trim() == '' || confirm.trim() == '') {
-			document.querySelector('.valid').textContent = 'Please fill the blanks!'
-			return
-		}  
-		if (password !== confirm){
-			document.querySelector('.valid').textContent = 'Password did not match'
-			setExistEmail(false)
-			return
+		e.preventDefault();
+		document.querySelector('.valid').textContent = '';
+		if (email.trim() == '' || password.trim() == '' || confirm.trim() == '') {
+			document.querySelector('.valid').textContent = 'Please fill the blanks!';
+			return;
 		}
-		if(users) {
-			users.forEach(user => {
-				if(user.email == email.trim()) {
-					document.querySelector('.valid').textContent = 'Email is already taken'
-					setExistEmail(false)
-					return
+		if (password !== confirm) {
+			document.querySelector('.valid').textContent = 'Password did not match';
+			setExistEmail(false);
+			return;
+		}
+		if (users) {
+			users.forEach((user) => {
+				if (user.email == email.trim()) {
+					document.querySelector('.valid').textContent = 'Email is already taken';
+					setExistEmail(false);
+					return;
 				}
-			})
-		} 
-		
-		if(existEmail) {
-			axios.post('http://localhost:4000/users', {
-						email:email,
-						password:password
-					})
-					.then(res => console.log(res.data)).catch(err => console.log(err))
-					document.querySelector('.valid').textContent = 'Signed up successfully!'
-					setExistEmail(true)
-					setConfirm('')
-					setEmail('')
-					setPassword('')
-					setRefresh(!refresh)
+			});
 		}
-		
-	}
+
+		if (existEmail) {
+			axios
+				.post('http://localhost:4000/users', {
+					email: email,
+					password: password
+				})
+				.then((res) => console.log(res.data))
+				.catch((err) => console.log(err));
+			document.querySelector('.valid').textContent = 'Signed up successfully!';
+			setExistEmail(true);
+			setConfirm('');
+			setEmail('');
+			setPassword('');
+			setRefresh(!refresh);
+		}
+	};
 
 	return (
 		<div className="sign-up">
 			<div className="sign-up-body">
 				<h1 className="sign-up-SignIn">Sign up</h1>
-				<h3 className="valid" style={{color:'red'}}></h3>
+				<h3 className="valid" style={{ color: 'red' }} />
 				<form className="sign-up-form" onSubmit={signup}>
 					<input
 						className="sign-up-email"
@@ -88,10 +99,25 @@ const SignUp = () => {
 						placeholder="Confirm your password"
 						onChange={(e) => setConfirm(e.target.value)}
 					/>
-					<button className="link" >Sign up</button>
-					<button className="sign-up-google" type="button">
-						<img src={googleLogo} alt="google logo" /> Join with Google
-					</button>
+					<button className="link">Sign up</button>
+					<GoogleLogin
+						render={(renderProps) => (
+							<button
+								className="sign-up-google"
+								type="button"
+								onClick={renderProps.onClick}
+								disabled={renderProps.disabled}
+							>
+								<img src={googleLogo} alt="google logo" /> Join with Google
+							</button>
+						)}
+						className="sign-up-google"
+						clientId="829612859214-rcj9ribrih4nt1vf7qkv7gqcrntuepak.apps.googleusercontent.com"
+						buttonText="Join with Google"
+						onSuccess={responseGoogle}
+						onFailure={responseGoogle}
+						cookiePolicy={'single_host_origin'}
+					/>
 				</form>
 				<div className="sign-up-login">
 					<p>
