0d6d1cc1a809e4936722a6aa5dd10a53be2ea742
Elif Balci <mrselifbalci@gmail.com>
2021-03-19 14:15:42 +0000

    db.json updated

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,30 +1,61 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
 import "./News.css";
 
 const News = () => {
+ 
+const[posts,setPosts]=useState([])
+const[medias,setMedias]=useState([])
+
+  useEffect(() => {
+    axios.get(`http://localhost:4000/posts`)
+    .then(res=>{ 
+        setPosts(res.data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+}, [])
+
+useEffect(() => {
+  axios.get(`http://localhost:4000/medias`)
+  .then(res=>{ 
+      setMedias(res.data)
+  })
+  .catch(err=>{
+      console.log(err)
+  })
+}, [])
+
+console.log(posts)
+
   return (
     <div>
       <div className="newss-bg-img"></div>
       <div className="newss-text">
         <span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.{" "}
+          Sabit bir soz
         </span>
       </div>
       <div className="newss-header">
-        <p>Latest News</p>
+        <p>Latest News</p>  
       </div>
       <div className="news-area">
         <div className="first-news">
-          <button className="news-btn-readmore">Read More</button>
+          
+          {posts.length !== 0 && posts[posts.length - 1].summary}  
+          {medias.map(media=>media.id===posts[posts.length - 1].post_img_Id ? 
+          <img src={media.media_url} alt=""/>:null
+          )} 
+          <Link to={`/newsdetail/${posts.length !== 0 && posts[posts.length - 1].id}`}  className="news-btn-readmore">Read More</Link>
         </div>
         <div className="first-news">
-          <button className="news-btn-readmore">Read More</button>
+          {/* <Link to={`/newsdetail`}  className="news-btn-readmore">Read More</Link> */}
         </div>
         <div>
-          <button id="news-btn-viewmore">View More</button>
+          {/* <button id="news-btn-viewmore">View More</button> */}
         </div>
       </div>
     </div>
---
+++ src/components/NewsDetail/NewsDetail.js
@@ -0,0 +1,40 @@
+import React,{useEffect,useState} from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios';
+
+export default function NewsDetail() {
+    
+    const{id}=useParams()
+    const[post,setPost]=useState([])
+    const[medias,setMedias]=useState([])
+    useEffect(() => {
+        axios.get(`http://localhost:4000/posts/${id}`)
+        .then(res=>{ 
+            setPost(res.data)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }, [])
+
+    
+    useEffect(() => {
+        axios.get(`http://localhost:4000/medias`)
+        .then(res=>{ 
+            setMedias(res.data)
+
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }, [])
+
+
+    return (
+        <div style={{height:'700px',margin:"110px"}}>
+            {medias.map(media=>media.id===post.post_img_Id ? <img src={media.media_url} alt='top'style={{height:"500px",width:"100%"}} />:null )}
+            <h1>{post.title}</h1> 
+            {medias.map(media=>media.id===post.donatee_img_id ? <img src={media.media_url} alt='top'style={{height:"100px",width:"100px"}} />:null )}
+    </div>
+    )
+}
