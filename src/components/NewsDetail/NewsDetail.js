56e70124db8f4e88b3f337a0b4749889c78f207a
Elif Balci <mrselifbalci@gmail.com>
2021-03-20 01:51:13 +0000

    NewsDetail.css

--- src/components/NewsDetail/NewsDetail.js
+++ src/components/NewsDetail/NewsDetail.js
@@ -2,6 +2,9 @@ import React,{useEffect,useState,useLayoutEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import axios from 'axios';
 import './NewsDetail.css'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ckeditor from '@ckeditor/ckeditor5-react';
+
 
 export default function NewsDetail() {
 
@@ -51,6 +54,9 @@ export default function NewsDetail() {
             <div className="title-summary" >
                     <h1> {post.title}</h1>
                     <p> {post.summary}</p>
+            </div>
+            <div style={{fontSize:"90px",color:"#347ca5",textAlign:"center",marginBottom:"200px"}}>
+               CONTENT FROM CK EDITOR
             </div>
             
         </div>
