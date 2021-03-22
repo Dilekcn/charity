956faa60f2281a4cc940d58f83b77e156e549cb9
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-22 04:49:21 +0000

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,58 @@
 import React from 'react';
 import logo from './images/img.png';
 import './Header.css';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Header = ({ isLoggedIn, setIsLoggedIn }) => {
 	return (
 		<div id="div-header">
-			<div>
-				<a href="/">
-					<img id="logo" src={logo} alt="logo" />
-				</a>
-			</div>
-			<div>
-				<a href="/donate">
-					<button id="btn-donate">DONATE</button>
-				</a>
-			</div>
-			<div>
-				<nav className="header-nav">
-					<ul>
-						<li>
-							<a href="/getInvolved">Get Involved</a>
-						</li>
-						<li>
-							<a href="/campaings&news">Campaigns & News</a>
-						</li>
-						<li>
-							<a href="/aboutus">About Us</a>
-						</li>
-						<li>
-							<a
-								href="/login"
-								onClick={(e) =>
-									e.target.textContent == 'Log Out' &&
-									setIsLoggedIn(false)
-								}
-							>
-								{isLoggedIn ? 'Log Out' : 'Log In'}
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			<div>
-				<input id="search-box" type="text" placeholder="Search" />
-			</div>
+			<a href="/">
+				<img id="logo" src={logo} alt="logo" />
+			</a>
+
+			<a href="/donate">
+				<button id="btn-donate">DONATE</button>
+			</a>
+			<form className="header-search-container">
+				<input
+					className="header-search-box"
+					type="search"
+					placeholder="Search"
+					required
+				/>
+				<button className="header-search-box-btn">
+					<AiOutlineSearch size={17} fill={'white'} />
+				</button>
+			</form>
+			<input type="checkbox" id="header-menu-checkbox" />
+			<label for="header-menu-checkbox" className="header-menu-icon">
+				<GiHamburgerMenu size={30} style={{ fill: '#347ca5' }} />
+			</label>
+			<nav className="header-nav">
+				<ul className="header-nav-links">
+					<li>
+						<a href="/getInvolved">Get Involved</a>
+					</li>
+					<li>
+						<a href="/campaings&news">Campaigns & News</a>
+					</li>
+					<li>
+						<a href="/aboutus">About Us</a>
+					</li>
+					<li>
+						<a
+							href="/login"
+							onClick={(e) =>
+								e.target.textContent == 'Log Out' &&
+								setIsLoggedIn(false)
+							}
+						>
+							{isLoggedIn ? 'Log Out' : 'Log In'}
+						</a>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 };
