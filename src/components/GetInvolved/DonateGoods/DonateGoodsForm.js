7a17736f7c599522f59356205ec2208f3ceaddaa
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-29 04:20:52 +0100

    updated donate goods form

--- src/components/GetInvolved/DonateGoods/DonateGoodsForm.js
+++ src/components/GetInvolved/DonateGoods/DonateGoodsForm.js
@@ -5,7 +5,7 @@ import Modal from 'react-modal';
 import { Link } from 'react-router-dom';
 
 const DonateGoodsForm = () => {
-	const [modalIsOpen, setmodalIsOpen] = useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
 	const [name, setName] = useState('');
 	const [phone, setPhone] = useState('');
 	const [email, setEmail] = useState('');
@@ -21,10 +21,10 @@ const DonateGoodsForm = () => {
 
 	const makeAnotherDonation = () => {
 		window.scroll(0, 0);
-		setmodalIsOpen(false);
+		setModalIsOpen(false);
 	};
 
-	const submitForm = () => {
+	const submitForm = (e) => {
 		setName('');
 		setPhone('');
 		setEmail('');
@@ -33,7 +33,8 @@ const DonateGoodsForm = () => {
 		setAddress('');
 		setPostCode('');
 		setInstructions('');
-		setmodalIsOpen(true);
+		setModalIsOpen(true);
+		e.preventDefault();
 	};
 
 	return (
@@ -49,108 +50,110 @@ const DonateGoodsForm = () => {
 				</div>
 			</div>
 			<div className="donate-goods-form-container">
-				<div className="donate-goods-form-left">
-					<form className="donate-goods-form-left">
-						<div>
-							<label>Full Name</label>
-							<input
-								type="text"
-								placeholder="Enter your full name"
-								value={name}
-								onChange={(e) => setName(e.target.value)}
-								required
-							/>
+				<form onSubmit={submitForm}>
+					<div className="donate-goods-form">
+						<div className="donate-goods-form-left">
+							<div>
+								<label>Full Name</label>
+								<input
+									type="text"
+									placeholder="Enter your full name"
+									value={name}
+									onChange={(e) => setName(e.target.value)}
+									required
+								/>
+							</div>
+							<div>
+								<label>Contact Number</label>
+								<input
+									type="text"
+									placeholder="Enter contact number"
+									value={phone}
+									onChange={(e) => setPhone(e.target.value)}
+									required
+								/>
+							</div>
+							<div>
+								<label>Email Address</label>
+								<input
+									type="email"
+									placeholder="Enter your email address"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									required
+								/>
+							</div>
+							<div>
+								<label>
+									Type of goods
+									<br /> (books,clothes etc.)
+								</label>
+								<input
+									type="text"
+									placeholder="Enter type of goods"
+									value={typeOfGoods}
+									onChange={(e) => setTypeOfGoods(e.target.value)}
+									required
+								/>
+							</div>
+							<div>
+								<label>Number of Pieces</label>
+								<input
+									type="text"
+									placeholder="Enter number of pieces"
+									value={numberOfPieces}
+									onChange={(e) => setNumberOfPieces(e.target.value)}
+									required
+								/>
+							</div>
 						</div>
-						<div>
-							<label>Contact Number</label>
-							<input
-								type="text"
-								placeholder="Enter contact number"
-								value={phone}
-								onChange={(e) => setPhone(e.target.value)}
-								required
-							/>
+						<div className="donate-goods-form-right">
+							<div>
+								<label>Address</label>
+								<textarea
+									type="text"
+									placeholder="Enter the full address"
+									value={address}
+									onChange={(e) => setAddress(e.target.value)}
+									required
+								/>
+							</div>
+							<div>
+								<label>Post Code</label>
+								<input
+									type="text"
+									placeholder="Enter the post code"
+									value={postCode}
+									onChange={(e) => setPostCode(e.target.value)}
+									required
+								/>
+							</div>
+							<div>
+								<label>Instructions for the Driver</label>
+								<textarea
+									type="email"
+									placeholder="Enter a clear instruction for the driver "
+									value={instructions}
+									onChange={(e) => setInstructions(e.target.value)}
+									required
+								/>
+							</div>
 						</div>
-						<div>
-							<label>Email Address</label>
-							<input
-								type="email"
-								placeholder="Enter your email address"
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-								required
-							/>
-						</div>
-						<div>
-							<label>
-								Type of goods
-								<br /> (books,clothes etc.)
-							</label>
-							<input
-								type="text"
-								placeholder="Enter type of goods"
-								value={typeOfGoods}
-								onChange={(e) => setTypeOfGoods(e.target.value)}
-								required
-							/>
-						</div>
-						<div>
-							<label>Number of Pieces</label>
-							<input
-								type="text"
-								placeholder="Enter number of pieces"
-								value={numberOfPieces}
-								onChange={(e) => setNumberOfPieces(e.target.value)}
-								required
-							/>
-						</div>
-					</form>
-				</div>
+					</div>
+					<div className="donate-goods-form-submit-container">
+						<button
+							type="submit"
+							className="donate-goods-form-submit-btn"
+						>
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
 
-				<div className="donate-goods-form-right">
-					<div>
-						<label>Address</label>
-						<textarea
-							type="text"
-							placeholder="Enter the full address"
-							value={address}
-							onChange={(e) => setAddress(e.target.value)}
-							required
-						/>
-					</div>
-					<div>
-						<label>Post Code</label>
-						<input
-							type="text"
-							placeholder="Enter the post code"
-							value={postCode}
-							onChange={(e) => setPostCode(e.target.value)}
-							required
-						/>
-					</div>
-					<div>
-						<label>Instructions for the Driver</label>
-						<textarea
-							type="email"
-							placeholder="Enter a clear instruction for the driver "
-							value={instructions}
-							onChange={(e) => setInstructions(e.target.value)}
-							required
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="donate-goods-form-submit-container">
-				<button
-					className="donate-goods-form-submit-btn"
-					onClick={() => submitForm()}
-				>
-					Submit
-				</button>
-			</div>
 			<Modal
 				isOpen={modalIsOpen}
-				onRequestClose={() => setmodalIsOpen(false)}
+				onRequestClose={() => setModalIsOpen(false)}
 				style={{
 					overlay: {},
 					content: {
