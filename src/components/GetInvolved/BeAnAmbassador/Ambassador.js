5575ee000bd1786f895078e9e2abcea19c679c82
Talha <noname_2338@hotmail.com>
2021-04-05 19:12:39 +0200

    ambassador pages responsive updated.

--- src/components/GetInvolved/BeAnAmbassador/Ambassador.js
+++ src/components/GetInvolved/BeAnAmbassador/Ambassador.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import imageHeader from '../DonateGoods/Rectangle 26.png';
 import "./Ambassador.css";
+import "./AmbassadorForm.css"
 import { Link } from 'react-router-dom';
 
 const Ambassador = () => {
@@ -8,15 +9,11 @@ const Ambassador = () => {
 		window.scroll(0, 0);
 	}, []);
   return (
-    <div className="donate-goods">
-      <div className="donate-goods-container">
-				<img
-					src={imageHeader}
-					alt="Avatar"
-					className="donate-goods-image"
-				/>
-				<div className="donate-goods-overlay">
-					<h1 className="donate-goods-text">Be an ambassador</h1>
+    <div>
+      <div className="ambassadorForm-container">
+				<img src={imageHeader} alt="Avatar" className="ambassadorForm-image" />
+				<div className="ambassadorForm-overlay">
+					<h1 className="ambassadorForm-text" id="h1-content">Be an Ambassador</h1>
 				</div>
 			</div>
       <div className="contentAmb">
@@ -26,10 +23,10 @@ const Ambassador = () => {
         <p>We at Helping Hands are passionate and excited about providing a platform for people to get engaged and inspired to be part of making a difference in the lives of others. Being part of this program will allow you the opportunity to have a say, make your voice count, and participate in new opportunities:</p>  
         <br/>
         <ul>
-          <li>- organising local and global initiatives to connect other people to HH’s amazing cause</li>
+          <li>- organising local and global initiatives to connect other people to HH’s amazing cause</li> <br/>
           <li>-sharing your story through online blogs and social media</li> <br/>
-          <li>-involvement in fundraising and awareness challenges</li>
-          <li>-dialogue and interaction with other  people through regular calls and online forums</li>
+          <li>-involvement in fundraising and awareness challenges</li> <br/>
+          <li>-dialogue and interaction with other  people through regular calls and online forums</li> <br/>
           <li>-learning and engaging through awareness trips to partner locations overseas</li>
         </ul>
       <br/>
