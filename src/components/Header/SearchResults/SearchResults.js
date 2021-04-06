345104f1073e6de2350d6e96178e365c804ce354
Elif Balci <mrselifbalci@gmail.com>
2021-04-06 21:17:58 +0100

    db.json updated

--- src/components/Header/SearchResults/SearchResults.js
+++ src/components/Header/SearchResults/SearchResults.js
@@ -30,16 +30,8 @@ const SearchResults = ({results, searchResults}) => {
 
 	return (
 		<div>
-			<div className="newss-bg-img"></div>
-			<div className="newss-text">
-				<span>
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry's standard dummy text
-					ever since the 1500s, when an unknown printer took a galley of
-					type and scrambled it to make a type specimen book.{' '}
-				</span>
-			</div>
-			<div className="newss-header">
+			
+			<div style={{marginTop:"100px"}} className="newss-header">
 				<p>Search Results</p>
 			</div>
 			<table id="news-area">
