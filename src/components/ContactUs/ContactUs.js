95f0452bcd9b53c3ab14656b55e097fad40e740d
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-04-07 22:05:19 +0100

    updated contact us page

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -2,6 +2,12 @@ import React, { useState, useLayoutEffect } from 'react';
 import './ContactUs.css';
 import imageHeader from '../GetInvolved/DonateGoods/Rectangle 26.png';
 import axios from 'axios';
+import {
+	AiFillFacebook,
+	AiFillTwitterSquare,
+	AiOutlineInstagram,
+} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const ContactUs = () => {
 	useLayoutEffect(() => {
@@ -44,43 +50,94 @@ const ContactUs = () => {
 					<h1 className="donate-goods-text">Contact Us</h1>
 				</div>
 			</div>
-			<h4 className="contact-us-intro-text">
-				Please complete this enquiry form. We will try our best to respond
-				within three working days.
-			</h4>
-			<div className="contact-us-form-container">
-				<form className="contact-us-form" onSubmit={submitMessage}>
-					<label>First Name</label>
-					<input
-						type="text"
-						placeholder="Enter your first name..."
-						onChange={(e) => setFirstName(e.target.value)}
-						required
-					/>
-					<label>Last Name</label>
-					<input
-						type="text"
-						placeholder="Enter your last name..."
-						onChange={(e) => setLastName(e.target.value)}
-						required
-					/>
-					<label>Email Address</label>
-					<input
-						type="email"
-						placeholder="Enter your email address..."
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
-					<label>Your Message</label>
-					<textarea
-						placeholder="Enter your message..."
-						onChange={(e) => setMessage(e.target.value)}
-						required
-					/>
-					<button type="submit" className="contact-us-form-submit-btn">
-						Submit
-					</button>
-				</form>
+			<div className="contact-us-container">
+				<div className="contact-us-form-container">
+					<h4 className="contact-us-intro-text">
+						Please complete this enquiry form. We will try our best to
+						respond within three working days.
+					</h4>
+					<form className="contact-us-form" onSubmit={submitMessage}>
+						<label>First Name</label>
+						<input
+							type="text"
+							placeholder="Enter your first name..."
+							onChange={(e) => setFirstName(e.target.value)}
+							required
+						/>
+						<label>Last Name</label>
+						<input
+							type="text"
+							placeholder="Enter your last name..."
+							onChange={(e) => setLastName(e.target.value)}
+							required
+						/>
+						<label>Email Address</label>
+						<input
+							type="email"
+							placeholder="Enter your email address..."
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+						<label>Your Message</label>
+						<textarea
+							placeholder="Enter your message..."
+							onChange={(e) => setMessage(e.target.value)}
+							required
+						/>
+						<button type="submit" className="contact-us-form-submit-btn">
+							Submit
+						</button>
+					</form>
+				</div>
+				<div className="contact-us-communication">
+					<div className="contact-us-communication-address">
+						<h2 className="contact-us-communication-address-title">
+							Address
+						</h2>
+						<h3 className="contact-us-communication-address-text">
+							Senate House Malet Street London WC1E 7HU
+						</h3>
+					</div>
+					<div className="contact-us-communication-phone">
+						<h2 className="contact-us-communication-phone-title">
+							Phone
+						</h2>
+						<h3 className="contact-us-communication-phone-text">
+							020 3451 5677
+						</h3>
+					</div>
+					<div className="contact-us-communication-follow-us">
+						<h2 className="contact-us-communication-follow-us-title">
+							Follow Us
+						</h2>
+						<div>
+							<a
+								href="https://www.facebook.com/"
+								target="_blank"
+								rel="noreferrer"
+								className="contact-us-communication-sm-logo-link"
+							>
+								<AiFillFacebook className="contact-us-communication-sm-logo" />
+							</a>
+							<a
+								href="https://www.twitter.com/"
+								target="_blank"
+								rel="noreferrer"
+								className="contact-us-communication-sm-logo-link"
+							>
+								<AiFillTwitterSquare className="contact-us-communication-sm-logo" />
+							</a>
+							<a
+								href="https://www.instagram.com/"
+								target="_blank"
+								rel="noreferrer"
+								className="contact-us-communication-sm-logo-link"
+							>
+								<AiOutlineInstagram className="contact-us-communication-sm-logo" />
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
